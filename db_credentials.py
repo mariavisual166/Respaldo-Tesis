542c079ecb4263840f77f6236289abcd96682791
mariavisual166 <mariavisual166@gmail.com>
2018-12-01 15:43:20 -0400

    componete actualizado

--- db_credentials.py
+++
@@ -1,7 +0,0 @@
-# postgresql 
-postgresql_db_config = {
-		'user': 'postgres',
-		'password': '123456',
-		'host': 'localhost',
-		'database': 'docente',
-	}
--- teachers/Teachers.vue
+++ teachers/Teachers.vue
@@ -6,15 +6,15 @@
         <div class="nav nav-item nav-pills text-center fondo"   role="tablist" aria-orientation="vertical">
           <a class="nav-link active" id="LinkArchivo" @click="cambiar2()"href="#" >CARGA DE ARCHIVO</a>
           <a class="nav-link" id="LinkFecha"   @click="cambiar()" href="#" >FECHAS TOPES</a>
+          <a class="nav-link" id="FechasA" @click="ultima()" href="#" >ULTIMAS ACTUALIZACIONES</a>
         </div>
       </div>
     </div
   <h3></h3>
   <div id="carga" >
-  <h2>Docentes</h2>
+  <h2>Carga de archivos con informacion docente de la UC</h2>
     <div class="large-12 medium-12 small-12 cell" >
       <div id= "i" class="alert alert-secondary" role="alert">
-      Carga de documentos con los datos docentes de la UC
       </div>
     </div>
         
@@ -30,21 +30,18 @@
           </div>
           <div class="col-xl-4 col-md-4 col-sm-0">
             <div class="intru">
-               <h4 >Pasos a seguir</h4>
-                <p >1-haga click en el boton seleccionar archivo.</p>
-                <p >2-seleccione el archivo csv que desea ingresar en el sistema.</p>
-                 <button class="btn btn-light" id="centar" @click="Ver()" >Ver ejemplo archivo </button>
-                <p >3-Presione el boton de enviar.</p>
-                <p >4-En caso que ocurra  un error , modifique el archivo y vuelva a intentarlo.</p>
+              <h4 >Ayuda</h4>
+              <p >¿No sabe que archivo subir?</p>
+              <button class="btn btn-light" id="centar" @click="Ver()" >Ver ejemplo archivo </button>     
+             
               </div>
           </div>
         </div>
   </div>
   <div id="fecha" >
-    <h2>Docentes</h2>
+    <h2>Fechas tope de subida de archivos</h2>
      <div class="large-12 medium-12 small-12 cell" >
       <div id= "e" class="alert alert-secondary" role="alert">
-      Carga de documentos con los datos docentes de la UC
       </div>
     </div>
                   <form class="col s12 margen">
@@ -108,6 +105,61 @@
      <button class="btn btn-light" id="centar" @click="Regresar()" >Regresar </button>
 
   </div>
+  <div id="UltimaFecha">
+    <h2>Ejemplo</h2>
+  <form class="col s12 margen">
+                      
+                      
+                      <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Faces</label>
+                        <div class="col-md-6">
+                          <input type="text"  id="fechaAFaces" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                    <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Facyt</label>
+                        <div class="col-md-6">
+                          <input type="text" id="fechaAFacyt" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Face</label>
+                        <div class="col-md-6">
+                          <input type="text" id="fechaAFace" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                    
+                    <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Fcs</label>
+                        <div class="col-md-6">
+                          <input type="text"  id="fechaADerecho" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                    
+                    <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Facultad de Odontologia</label>
+                        <div class="col-md-6">
+                          <input type="text"  id="fechaAOdontologia" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Ingieneria</label>
+                        <div class="col-md-6">
+                          <input type="text"  id="fechaAIngieneria" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="first_name" class="col-sm-4 col-form-label text-md-right">Fecha tope de subida Fcjp</label>
+                        <div class="col-md-6">
+                          <input type="text" id="fechaAFcjp" value="2010-10-01" disabled class="form-control" >
+                        </div>
+                      </div>
+                    
+                      
+                  </form>
+     <button class="btn btn-light" id="centar" @click="Regresar()" >Regresar </button>
+
+  </div>
 </div>
 
 
@@ -116,7 +168,7 @@
 <script>
 
 
-
+import jsPDF from "jsPDF";
 import axios from 'axios';
 
   export default {
@@ -127,6 +179,7 @@ import axios from 'axios';
       return {
         file: '',
         fecha: '2010-01-01',
+        Ultima:'',
       }
     },
 
@@ -159,10 +212,12 @@ import axios from 'axios';
           var con = document.getElementById("e");
           con.textContent  = res.data["exitosa"];
 
-
-         $( "#e" ).addClass( "verde" )
-          console.log(res.data); 
-          console.log('SUCCESS!!');
+       
+             $( "#e" ).addClass( "verde" )
+              console.log(res.data); 
+              console.log('SUCCESS!!');
+           
+       
          
         })
         .catch(res=> {
@@ -200,9 +255,10 @@ import axios from 'axios';
               }
             ).then(function(res){
             var div = document.getElementById("i");
+            var i=res.data["Erros"].length;
+            div.textContent  = res.data["Erros"][i-1]["exitosa"];
 
-           div.textContent  = res.data["exitosa"];
-           if(res.data["exitosa"]== "El Archivo csv ha sido procesado con exito!!!"){
+           if(res.data["Erros"][i-1]["exitosa"]== "El Archivo csv ha sido procesado con exito!!!"){
             $( "#archi" ).removeClass( "rojo" )
             $( "#archi" ).removeClass( "normal" )
             $( "#archi" ).addClass( "verde" )
@@ -211,12 +267,61 @@ import axios from 'axios';
             $('#enviar').attr("disabled", true); 
            }else
            {
+
             $( "#archi" ).removeClass( "normal" ).addClass( "rojo" )
             $( "#i" ).removeClass( "alert alert-secondary" ).addClass( "alert alert-danger" )
            }
+           if(res.data["Erros"][i-1]["exitosa"]== "Error a procesar el archivo. Por favor revise el pdf LISTA_ERRORES para conocer los errores del archivo ingresado al sistema")
+          {
+            var doc = new jsPDF("l", "mm", "a4");
+            doc.setFont("helvetica");
+            doc.setFontType("bold");
+            doc.setFontSize(12);
+            doc.setProperties({
+              title: "Lista de errores",
+              subject: "Reporte",
+              author: "UC Ranking",
+              date: new Date()
+            });
+            let y = 30
+            let index = 0
+            doc.text(140, 20, "Lista de errores");
+            res.data["Erros"].forEach(() => {
+              if(index !==  res.data["Erros"].length - 1) {
+                doc.text(20, y, res.data["Erros"][index]["error"]);
+                y += 10
+                index += 1
+              }
+            })
+
+            doc.addPage();
+            doc.text(140, 20, "Sugerencias");
+            doc.text(20, 40, "la columna Cedula solo puede contener valores numericos");
+            doc.text(20, 50, "las columnas primer_nombre,segundo_nombre,primer_apellido,area de invetigacion, titulo, y segundo_apellido   solo puede  ");
+            doc.text(20, 60, "contener valores Alfabeticos");
+            doc.text(20, 70, "la columna sexo solo puede contener los valores F (Femenino) o M (Masculino)");
+            doc.text(20, 80, "la los campos de la columna correo solo debe tener el formato @example.com o @uc.edu.ve");
+          
+            doc.text(20, 90, "la columna nacionalidad solo puede contener los valores V (Venezolano) o E (Extranjero)");
+            doc.text(20, 100, "la columna tipo solo puede contener los valores Normal, Contratado o Investigador");
+            doc.text(20, 110, "la columna area de invetigacion solo puede contener los valores Normal, Contratado o Investigador");
+            doc.text(20, 120, "la columna tipo solo puede contener los valores Normal, Contratado o Investigador");
+            doc.text(20, 130,"la columna Nivel solo puede contener los valores Pregrado, Postgrado, Maestia, Doctodado");
+            doc.text(20, 140,"la columna escalafon solo puede contener los valores Instructor ,Asistente, Agregado,Asociado,Titular");
+            doc.text(20, 150,"la columna escalafon solo puede contener los valores Faces, Facyt, Face, Fcs, Odontologia,Fcjp y Ingieneria");
+              //Info for verification
+          
+            doc.setFont("helvetica");
+            doc.setFontType("bold");
+            doc.setFontSize(16);
+            
+
+            doc.save("LISTA_ERRORES" + ".pdf");
+             
+          }
 
          
-          console.log(res.data); 
+          console.log(res.data["Erros"][i-1]["exitosa"]); 
           console.log('SUCCESS!!');
         })
         .catch(res=> {
@@ -225,11 +330,13 @@ import axios from 'axios';
         });
       }, cambiar()
       {
-      $( "#LinkArchivo" ).removeClass( "active" )
-      $( "#LinkFecha" ).addClass( "active" )
-      $("#carga").css("display", "none");
-      $("#fecha").css("display", "block");
-        console.log("HOLA");
+        $( "#FechasA" ).removeClass( "active" )
+        $( "#LinkArchivo" ).removeClass( "active" )
+        $( "#LinkFecha" ).addClass( "active" )
+        $("#carga").css("display", "none");
+        $("#UltimaFecha").css("display", "none");
+        $("#fecha").css("display", "block");
+       
       },
       Ver() {
       $("#submenu").css("display", "none");
@@ -240,6 +347,8 @@ import axios from 'axios';
       },
       cambiar2()
       {
+       $("#UltimaFecha").css("display", "none");
+       $( "#FechasA" ).removeClass( "active" )
       $( "#LinkFecha" ).removeClass( "active" )
       $( "#LinkArchivo" ).addClass( "active" )
       $("#fecha").css("display", "none");
@@ -249,7 +358,51 @@ import axios from 'axios';
       {
       $("#submenu").css("display", "block");
       $("#Ver").css("display", "none");
+      $("#UltimaFecha").css("display", "none");
       $("#carga").css("display", "block");
+      }, ultima()
+      {
+        $( "#LinkFecha" ).removeClass( "active" )
+        $( "#LinkArchivo" ).removeClass( "active" )
+        $( "#FechasA" ).addClass( "active" )
+        $("#fecha").css("display", "none");
+        $("#carga").css("display", "none");
+        $("#Ver").css("display", "none");
+         $("#UltimaFecha").css("display", "block");
+          
+          let formData = new FormData();
+           const path = 'http://localhost:8083/ultimaFecha' ;
+        
+             axios.post(path,
+                  formData,
+                  {
+                  headers: {
+                      'Content-Type': 'multipart/form-data'
+                  }
+                  
+                }
+              ).then(function(res){
+            
+           
+
+            $(fechaAFaces).val(res.data["Faces"])
+            $(fechaAFacyt).val(res.data["Facyt"])
+            $(fechaAFace).val(res.data["Face"])
+            $(fechaADerecho).val(res.data["Fcs"])
+            $(fechaAOdontologia).val(res.data["Odontologia"])
+            $(fechaAFcjp).val(res.data["Fcjp"])
+            $(fechaAIngieneria).val(res.data["Ingieneria"])
+               
+            console.log(res.data); 
+            console.log('SUCCESS!!');
+           
+          })
+          .catch(res=> {
+          console.log(res);
+          console.log('FAILURE!!');
+          });
+
+
       },
       /*
         Handles a change on the file upload
@@ -288,7 +441,7 @@ import axios from 'axios';
            border: 1px solid rgba(155,155,155,0.1);
          
         } 
-      #fecha,#Ver{
+      #fecha,#Ver,#UltimaFecha{
            display:none;
          
         }
@@ -315,7 +468,7 @@ import axios from 'axios';
       h5{
       margin-bottom:15px;
       text-align: center;
-      opacity :0.7;
+      opacity :0.0;
         }
         #enviar {
        
